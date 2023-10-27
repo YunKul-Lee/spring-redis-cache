b3502dd5e0bda863b2b0691615d767461b5d7f2d
jake <gerion@gmail.com>
2023-10-27 20:17:18 +0900

    application.yml 설정추가

--- src/main/java/com/jake/springrediscache/SpringRedisCacheApplication.java
+++ src/main/java/com/jake/springrediscache/SpringRedisCacheApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringRedisCacheApplication {
 
+    // https://github.com/aedemirsen/countries-be/tree/redis-cache
     public static void main(String[] args) {
         SpringApplication.run(SpringRedisCacheApplication.class, args);
     }
