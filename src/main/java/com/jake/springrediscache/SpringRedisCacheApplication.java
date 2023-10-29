bf6a64336e1056d7c3c1dd10590c4d267687600f
jake <gerion@gmail.com>
2023-10-29 21:39:26 +0900

    .

--- src/main/java/com/jake/springrediscache/SpringRedisCacheApplication.java
+++ src/main/java/com/jake/springrediscache/SpringRedisCacheApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringRedisCacheApplication {
 
-    // https://github.com/aedemirsen/countries-be/tree/redis-cache
     public static void main(String[] args) {
         SpringApplication.run(SpringRedisCacheApplication.class, args);
     }
